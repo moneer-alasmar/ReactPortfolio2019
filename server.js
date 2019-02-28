b41992c92628c79e2f8acee7eaa45f353fc513fe
Moneer <kiraalasmar@gmail.com>
2019-02-28 15:51:01 -0800

    heroku prep

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use("/static", express.static(path.join(__dirname, "client/build")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client/build", "index.html"));
