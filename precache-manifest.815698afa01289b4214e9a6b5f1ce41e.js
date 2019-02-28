d1c2d970e63de7210142bcbceb4bc52784abd60c
Moneer <kiraalasmar@gmail.com>
2019-02-28 12:28:07 -0800

    Updates

---
+++ precache-manifest.815698afa01289b4214e9a6b5f1ce41e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4469bfdc12ebdc5ed55d",
+    "url": "/ReactPortfolio2019/static/css/main.051b0d7f.chunk.css"
+  },
+  {
+    "revision": "4469bfdc12ebdc5ed55d",
+    "url": "/ReactPortfolio2019/static/js/main.4469bfdc.chunk.js"
+  },
+  {
+    "revision": "b38f3da2d8b1c7224b3e",
+    "url": "/ReactPortfolio2019/static/js/runtime~main.b38f3da2.js"
+  },
+  {
+    "revision": "741fef38289f8acbbf3d",
+    "url": "/ReactPortfolio2019/static/js/2.741fef38.chunk.js"
+  },
+  {
+    "revision": "a90f72eeca9d577bc39c866ea19f1212",
+    "url": "/ReactPortfolio2019/static/media/portrait.a90f72ee.jpg"
+  },
+  {
+    "revision": "f3dc422e0694beaab396ca5cc3805746",
+    "url": "/ReactPortfolio2019/static/media/Moneer Alasmar Full Stack Developer.f3dc422e.pdf"
+  },
+  {
+    "revision": "204e1e3298b982ba5cae99a60e12f3a5",
+    "url": "/ReactPortfolio2019/static/media/Background.204e1e32.jpg"
+  },
+  {
+    "revision": "279af88c4e69661479bd789221c4c396",
+    "url": "/ReactPortfolio2019/index.html"
+  }
+];
