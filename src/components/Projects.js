86183f9d6915197833461d490fd07da075a50e86
Moneer Alasmar <kiraalasmar@gmail.com>
2019-03-07 15:39:44 -0800

    completed projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,7 @@
 import React from "react";
 import myProjects from "../apis/projects.json";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
 
 class Projects extends React.Component {
   state = {
@@ -20,52 +22,79 @@ class Projects extends React.Component {
       });
       this.setState({ selectedProjects: selected });
     }
+    if (language === "all") {
+      this.setState({ selectedProjects: [] });
+    }
   };
 
   renderContent = () => {
     if (this.state.selectedProjects.length === 0) {
-      return this.state.projects.map(project => {
-        return (
-          <div key={project.title}>
-            <h1>{project.title}</h1>
-            <img src={project.image} alt="" />
-          </div>
-        );
-      });
-    }
-    return this.state.selectedProjects.map(project => {
       return (
-        <div key={project.title}>
-          <h1>{project.title}</h1>
-          <img src={project.image} alt="" />
-        </div>
+        <Carousel showThumbs={false}>
+          {this.state.projects.map(project => {
+            return (
+              <div key={project.title}>
+                <a href={project.deployLink}>
+                  <h4>{project.title}</h4>
+                </a>
+                <a href={project.githubLink}>Github</a>
+                <img src={project.image} alt={project.title} />
+              </div>
+            );
+          })}
+        </Carousel>
       );
-    });
+    }
+    return (
+      <Carousel showThumbs={false}>
+        {this.state.selectedProjects.map(project => {
+          return (
+            <div key={project.title}>
+              <a href={project.deployLink}>
+                <h4>{project.title}</h4>
+              </a>
+              <a href={project.githubLink}>Github</a>
+              <img src={project.image} alt={project.title} />
+            </div>
+          );
+        })}
+      </Carousel>
+    );
   };
 
   render() {
-    console.log(this.state);
     return (
       <div className="container">
         <div className="row">
-          <div className="col-md-12">
-            <div className="jumbotron">
-              <h1>Projects</h1>
+          <div className="col-md-2" />
+          <div className="col-md-8">
+            <div className="jumbotron text-center">
+              <p className="display-4">My Apps</p>
+              <p className="lead">Filter by Technology: </p>
               <button
                 onClick={() => this.selectLanguage("react")}
-                className="btn-primary"
+                className="btn btn-primary mr-2"
               >
+                <i className="fab fa-react mr-1" />
                 React
               </button>
               <button
                 onClick={() => this.selectLanguage("node")}
-                className="btn-primary"
+                className="btn btn-primary mr-2"
               >
+                <i className="fab fa-node-js mr-1" />
                 Node
               </button>
+              <button
+                onClick={() => this.selectLanguage("all")}
+                className="btn btn-primary"
+              >
+                Clear
+              </button>
+              <div style={{ paddingTop: 10 }}>{this.renderContent()}</div>
             </div>
-            <div className="jumbotron">{this.renderContent()}</div>
           </div>
+          <div className="col-md-2" />
         </div>
       </div>
     );
